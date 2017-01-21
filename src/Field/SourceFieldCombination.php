b6725276efba1df7a57bba0a83940ac4c2675443
Ashoka de Wit <sjokki@gmail.com>
2017-01-21 14:46:48 +0800

    Implement source field combination

---
+++ src/Field/SourceFieldCombination.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright WideFocus. All rights reserved.
+ * http://www.widefocus.net
+ */
+
+namespace WideFocus\Feed\Source\Field;
+
+/**
+ * Contains a combination of fields.
+ */
+class SourceFieldCombination implements SourceFieldCombinationInterface
+{
+    use SourceFieldTrait;
+    use SourceFieldCombinationTrait;
+
+    /**
+     * Get the field values for an entity.
+     *
+     * @param string $entityId
+     *
+     * @return array
+     */
+    public function getValue(string $entityId): array
+    {
+        return array_map(
+            function (SourceFieldInterface $field) use ($entityId) {
+                return $field->getValue($entityId);
+            },
+            $this->getFields()
+        );
+    }
+
+    /**
+     * Prepare for a set of entities.
+     *
+     * @param string[] $entityIds
+     *
+     * @return void
+     */
+    public function prepare(array $entityIds)
+    {
+        $fields = $this->getFields();
+        array_walk(
+            $fields,
+            function (SourceFieldInterface $field) use ($entityIds) {
+                $field->prepare($entityIds);
+            }
+        );
+    }
+}
