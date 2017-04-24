d0c73f224494df3330b79e2abf3e20ddb602087c
Ashoka de Wit <sjokki@gmail.com>
2017-04-24 12:38:51 +0200

    Move factory aggregates from deprecated builder package

---
+++ src/Field/FactoryAggregate/InvalidSourceFieldException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright WideFocus. See LICENSE.txt.
+ * https://www.widefocus.net
+ */
+
+namespace WideFocus\Feed\Source\Field\FactoryAggregate;
+
+use InvalidArgumentException;
+
+/**
+ * Exception thrown when a requested field does not exist.
+ */
+class InvalidSourceFieldException extends InvalidArgumentException
+{
+    /**
+     * Create an exception for a field that has not been registered.
+     *
+     * @param string $type
+     */
+    public function __construct(string $type)
+    {
+        parent::__construct(
+            sprintf(
+                'A source field with name %s has not been registered',
+                $type
+            )
+        );
+    }
+}
---
+++ src/IdentitySource/FactoryAggregate/InvalidIdentitySourceException.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright WideFocus. See LICENSE.txt.
+ * https://www.widefocus.net
+ */
+
+namespace WideFocus\Feed\Source\IdentitySource\FactoryAggregate;
+
+use InvalidArgumentException;
+
+/**
+ * Exception thrown when a requested identity source does not exist.
+ */
+class InvalidIdentitySourceException extends InvalidArgumentException
+{
+    /**
+     * Create an exception for a source that has not been registered.
+     *
+     * @param string $type
+     */
+    public function __construct(string $type)
+    {
+        parent::__construct(
+            sprintf(
+                'An identity source with name %s has not been registered',
+                $type
+            )
+        );
+    }
+}
